9caf7e7ab6e3f8ae334705f3d88a471f0fabff31
yuw <yuw@medimmune.com>
2016-08-03 18:10:37 -0400

    Multiple engine alignment

--- src/org/ms2ms/data/ms/PeptideEntry.java
+++ src/org/ms2ms/data/ms/PeptideEntry.java
@@ -1,7 +1,27 @@
 package org.ms2ms.data.ms;
 
-/**
+import org.expasy.mzjava.core.ms.spectrum.IonType;
+
+/** An entry of peptide without the actual sequence or mass information
+ *
  * Created by yuw on 8/3/16.
  */
-public class PeptideEntry {
+public class PeptideEntry
+{
+  private IonType mIonType; // enum
+  private int mSequencePointer, mTerminal;
+
+  public PeptideEntry() { super(); }
+  public PeptideEntry(int pointer, int terminal, IonType type)
+  {
+    super();
+    mSequencePointer=pointer; mTerminal=terminal; mIonType=type;
+  }
+
+  public int     getSequencePointer() { return mSequencePointer; }
+  public int     getTerminal()        { return mTerminal; }
+
+  public IonType getIonType()         { return mIonType; }
+  public boolean isY()                { return IonType.y==mIonType; }
+  public boolean isB()                { return IonType.b==mIonType; }
 }
