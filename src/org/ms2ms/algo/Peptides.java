cf4d659712951ccd6ce37c223f342767fa328634
yuw <yuw@medimmune.com>
2016-08-03 18:09:42 -0400

    Multiple engine alignment

--- src/org/ms2ms/algo/Peptides.java
+++ src/org/ms2ms/algo/Peptides.java
@@ -14,9 +14,7 @@ import org.expasy.mzjava.proteomics.ms.ident.PeptideMatch;
 import org.ms2ms.utils.Strs;
 import org.ms2ms.utils.Tools;
 
-import java.util.Collection;
-import java.util.EnumSet;
-import java.util.List;
+import java.util.*;
 
 /**
  * Created by yuw on 9/14/2015.
@@ -53,5 +51,23 @@ public class Peptides
         if (sAA.indexOf(c)>=0) peptide=Strs.extend(peptide, c+"","");
     }
     return peptide;
+  }
+  // setup a simple mapping of the AA symbol and their incremental masses
+  public static Map<Character, Double> newAAsMass(String fixed)
+  {
+    Map<Character, Double> AAs = new HashMap<>();
+    AAs.put('G', 57.02146d );AAs.put('A', 71.03711d );AAs.put('S', 87.03203d );AAs.put('P', 97.05276d );AAs.put('V', 99.06841d);
+    AAs.put('T', 101.04768d);AAs.put('L', 113.08406d);AAs.put('N', 114.04293d);AAs.put('D', 115.02694d);AAs.put('Q', 128.05858d);
+    AAs.put('E', 129.04259d);AAs.put('M', 131.04049d);AAs.put('H', 137.05891d);AAs.put('F', 147.06841d);AAs.put('R', 156.10111d);
+    AAs.put('C', 160.03065d);AAs.put('Y', 163.06333d);AAs.put('W', 186.07931d);AAs.put('K', 128.09496d);
+    AAs.put('^', 1.00783d  ); // N-terminal mod
+    AAs.put('$', 17.00273d ); // C-terminal mod
+
+    if ("tmt10".equalsIgnoreCase(fixed) || "tmt6".equalsIgnoreCase(fixed))
+    {
+      AAs.put('K', 357.25789d);
+      AAs.put('^', 229.162932d); // TMT-10
+    }
+    return AAs;
   }
 }
