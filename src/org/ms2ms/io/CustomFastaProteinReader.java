5b97b95b5cfd343e7392e2751046e0c962973560
yuw <yuw@medimmune.com>
2017-01-15 08:22:21 -0500

    qvals determination

---
+++ src/org/ms2ms/io/CustomFastaProteinReader.java
@@ -0,0 +1,31 @@
+package org.ms2ms.io;
+
+import org.expasy.mzjava.proteomics.io.mol.FastaProteinReader;
+import org.expasy.mzjava.proteomics.mol.Protein;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.Reader;
+
+/**
+ * Created by yuw on 1/14/17.
+ */
+public class CustomFastaProteinReader extends FastaProteinReader
+{
+  public CustomFastaProteinReader(Reader reader)
+  {
+    super(reader);
+  }
+
+  public CustomFastaProteinReader(File file) throws FileNotFoundException
+  {
+    super(file);
+  }
+
+  @Override
+  protected void parseHeader(String header, Protein.Builder builder)
+  {
+    int left = header.indexOf('>')+1, right = header.indexOf(' ');
+    builder.setAccessionId((right>0?header.substring(left, right):header.substring(left)).replaceAll("\\|", "_"));
+  }
+}
