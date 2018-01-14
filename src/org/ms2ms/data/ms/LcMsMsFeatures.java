7e3023e1fc75977aae1679d326cfc6759d451a6f
Yu, Wen <yuw@MedImmune.com>
2018-01-14 08:27:26 -0500

    20180113

--- src/org/ms2ms/data/ms/LcMsMsFeatures.java
+++ src/org/ms2ms/data/ms/LcMsMsFeatures.java
@@ -29,6 +29,10 @@ public class LcMsMsFeatures implements Binary
   public static final String COL_RT         = "rt";
   public static final String COL_RT_BOUND   = "rt range";
   public static final String COL_MASS       = "mass";
+  public static final String COL_MASS_CAL   = "calc mass";
+  public static final String COL_MASS_OBS   = "obs mass";
+  public static final String COL_PPM_PREC   = "precision in ppm";
+  public static final String COL_PPM        = "ppm";
   public static final String COL_SCAN       = "Scan number";
   public static final String COL_SCAN_BOUND = "Scan range";
   public static final String COL_AI_APEX    = "intensityApex";
@@ -38,6 +42,8 @@ public class LcMsMsFeatures implements Binary
   public static final String COL_PROT       = "protein";
   public static final String COL_MOD        = "mods";
   public static final String COL_PSMID      = "psmID";
+  public static final String COL_FDR        = "fdr";
+  public static final String COL_SCORE      = "score";
   public static final String COL_PROPERTY   = "Properties";
 
   static
@@ -82,13 +88,11 @@ public class LcMsMsFeatures implements Binary
       readMaxquantMS1(     new TabFile(mq+"/msScans.txt", "\t"));
       readMaxquantMsMs(    new TabFile(mq+"/msmsScans.txt", "\t"));
       readMaxquantFeatures(new TabFile(mq+"/allPeptides.txt", "\t"));
+      readMaxquantEvidence(new TabFile(mq+"/evidence.txt", "\t"));
+
+      Peptides2Ions();
 
       readDinosauer(lcms);
-
-//      System.out.println(mIons.size());
-
-//      Ions2Peptides(mz);
-//      Peptides2MsMs(mz);
     }
     catch (IOException ie) { }
   }
@@ -125,7 +129,7 @@ public class LcMsMsFeatures implements Binary
     if (mMsMs==null)  mMsMs = TreeBasedTable.create();
     int counts=0; mScan_MS2 = MultiTreeTable.create();
 
-    System.out.println("Reading the features from "+features.getFileName());
+    System.out.println("Reading the MS/MS features from "+features.getFileName());
     while (features.hasNext())
     {
       if (!okRun(features.get("Raw file"))) continue;
@@ -135,35 +139,113 @@ public class LcMsMsFeatures implements Binary
       // Fraction of total spectrum      Base peak fraction      Precursor full scan number      Precursor intensity     Precursor apex fraction Precursor apex offset
       // Precursor apex offset time      Scan event number       Modifications   Modified sequence       Proteins        Score   Intens Comp Factor      CTCD Comp       RawOvFtT
       // AGC Fill        Scan index      MS scan index   MS scan number
-      //Map<String, String> row  = features.nextRow();
 
-      Double mz = features.getDouble("m/z"), rt = features.getDouble("Retention time");
-      // add the features
-      Features F = new Features();
-      F.add(COL_MZ,       mz);
-      F.add(COL_RT,       rt);
-      F.add(COL_SCAN,     features.getInt("Scan number"));
-      F.add(COL_Z,        features.getInt("Charge"));
-      F.add(COL_MASS,     features.getDouble("Mass"));
-      F.add(COL_AI_APEX,  features.getDouble("Precursor intensity"));
-      F.add(COL_RUN,      features.get("Raw file"));
+      Features F = addMaxquantPSM(addMaxquantCore(new Features(), features), features);
 
-      if (features.getInt("MS/MS IDs")>0)
-      {
-        F.add(COL_PSMID, features.getInt("MS.MS.IDs"));
-        F.add(COL_SEQ, features.get("Sequence"));
-        F.add(COL_PROT, features.get("Proteins"));
-        if (!"Unmodified".equals(features.get("Modifications"))) F.add(COL_MOD, features.get("Modifications"));
-      }
+//      Double mz = features.getDouble("m/z"), rt = features.getDouble("Retention time");
+//      // add the features
+//      Features F = new Features();
+//      F.add(COL_MZ,       mz);
+//      F.add(COL_RT,       rt);
+//      F.add(COL_SCAN,     features.getInt("Scan number"));
+//      F.add(COL_Z,        features.getInt("Charge"));
+//      F.add(COL_MASS,     features.getDouble("Mass"));
+//      F.add(COL_AI_APEX,  features.getDouble("Precursor intensity"));
+//      F.add(COL_RUN,      features.get("Raw file"));
 
-      mMsMs.put(mz,rt, F);
-      mScan_MS2.put(features.getInt("Scan number"), features.get("Raw file"), F);
+//      if (features.getInt("MS/MS IDs")>0)
+//      {
+//        F.add(COL_PSMID, features.getInt("MS/MS IDs"));
+//        F.add(COL_SEQ, features.get("Sequence"));
+//        F.add(COL_PROT, features.get("Proteins"));
+//        if (!"Unmodified".equals(features.get("Modifications"))) F.add(COL_MOD, features.get("Modifications"));
+//      }
+
+//      mMsMs.put(mz,rt, F);
+      mMsMs.put(    F.getDouble(COL_MZ),   F.getDouble(COL_RT), F);
+      mScan_MS2.put(F.getInt(   COL_SCAN), F.getStr(  COL_RUN), F);
 
       if (++counts%10000==0) System.out.print(".");
       if (counts%1000000==0) System.out.println();
     }
     System.out.println(counts); features.close();
   }
+  private Features addMaxquantCore(Features F, TabFile features)
+  {
+    // add the features
+    F.add(COL_MZ,       features.getDouble("m/z"));
+    F.add(COL_RT,       features.getDouble("Retention time"));
+    F.add(COL_Z,        features.getInt("Charge"));
+    F.add(COL_MASS,     features.getDouble("Mass"));
+    F.add(COL_PPM_PREC, features.getDouble("Mass precision [ppm]"));
+
+    F.add(COL_SCAN,     features.getInt("Scan number"));
+    F.add(COL_Z,        features.getInt("Charge"));
+    F.add(COL_MASS,     features.getDouble("Mass"));
+    F.add(COL_AI_APEX,  features.getDouble("Precursor intensity"));
+    F.add(COL_RUN,      features.get("Raw file"));
+
+    if (features.getInt("Min scan number")!=null && features.getInt("Max scan number")!=null)
+      F.add(COL_RT_BOUND, Range.closed(
+        mScan_RT.get(features.getInt("Min scan number"), features.get("Raw file")),
+        mScan_RT.get(features.getInt("Max scan number"), features.get("Raw file"))));
+
+    if (features.getInt("Min scan number")!=null && features.getInt("Max scan number")!=null)
+      F.add(COL_SCAN_BOUND, Range.closed(features.getInt("Min scan number"), features.getInt("Max scan number")));
+
+    return F;
+  }
+  private Features addMaxquantPSM(Features F, TabFile features)
+  {
+    F.add(COL_PSMID,   features.get("MS/MS IDs"));
+    if (Strs.isSet(features.get("Sequence")))
+    {
+      F.add(COL_SEQ,     features.get("Sequence"));
+      F.add(COL_PEPTIDE, features.get("Modified sequence"));
+      F.add(COL_PROT,    features.get("Proteins"));
+      if (!"Unmodified".equals(features.get("Modifications"))) F.add(COL_MOD, features.get("Modifications"));
+
+      F.add(COL_FDR,     features.getDouble("PEP"));
+      F.add(COL_SCORE,   features.getDouble("Score"));
+    }
+
+    return F;
+  }
+  public void readMaxquantEvidence(TabFile features) throws IOException
+  {
+    if (mPSMs==null) mPSMs = new TreeMap<>();
+
+    int counts=0;
+
+    System.out.println("Reading the evidence features from "+features.getFileName());
+    while (features.hasNext())
+    {
+      if (!okRun(features.get("Raw file"))) continue;
+
+      // Sequence        Length  Modifications   Modified sequence       Deamidation (NQ) Probabilities  Oxidation (M) Probabilities     Deamidation (NQ) Score Diffs
+      // Oxidation (M) Score Diffs       Acetyl (N-term) Deamidation (NQ)        Oxidation (M)   Missed cleavages        Proteins        Leading proteins
+      // Leading razor protein   Gene names      Protein names   Type    Raw file        MS/MS m/z       Charge  m/z     Mass    Resolution      Uncalibrated - Calibrated m/z [ppm]
+      // Uncalibrated - Calibrated m/z [Da]      Mass error [ppm]        Mass error [Da] Uncalibrated mass error [ppm]   Uncalibrated mass error [Da]    Max intensity m/z 0
+      // Retention time  Retention length        Calibrated retention time       Calibrated retention time start Calibrated retention time finish        Retention time calibration
+      // Match time difference  Match m/z difference    Match q-value   Match score     Number of data points   Number of scans Number of isotopic peaks        PIF
+      // Fraction of total spectrum      Base peak fraction      PEP     MS/MS count     MS/MS scan number       Score   Delta score     Combinatorics   Intensity
+      // Reverse Potential contaminant   id      Protein group IDs       Peptide ID      Mod. peptide ID MS/MS IDs       Best MS/MS      AIF MS/MS IDs   Deamidation (NQ) site IDs
+      // Oxidation (M) site IDs
+
+      // add the features
+      Features F = addMaxquantPSM(addMaxquantCore(new Features(), features), features);
+      F.add(COL_PPM,  features.getDouble("Mass error [ppm]"));
+
+      if (F.get(COL_PSMID)!=null)
+        for (String id : Strs.split(F.getStr(COL_PSMID), ';'))
+          if (!"".equals(id)) mPSMs = Tools.put(mPSMs, Long.parseLong(id), F);
+
+      if (++counts%10000==0) System.out.print(".");
+      if (counts%1000000==0) System.out.println();
+    }
+    System.out.println(counts); features.close();
+  }
+
   public void readMaxquantMS1(TabFile features) throws IOException
   {
     System.out.println("Reading the MS1 scans from "+features.getFileName()); int counts=0;
@@ -201,29 +283,36 @@ public class LcMsMsFeatures implements Binary
       // Max intensity m/z 0     Retention time  Retention length        Retention length (FWHM) Min scan number
       // Max scan number Identified      MS/MS IDs       Sequence        Length  Modifications   Modified sequence
       // Proteins        Score   Intensity       Intensities     Isotope pattern MS/MS Count     MSMS Scan Numbers       MSMS Isotope Indices
-      Double mz = features.getDouble("m/z"), rt = features.getDouble("Retention time");
-      // add the features
-      Features F = new Features();
-
-      F.add(COL_MZ, mz).add(COL_RT, rt);
-      F.add(COL_Z,        features.getInt("Charge"));
-      F.add(COL_MASS,     features.getDouble("Mass"));
+      Features F = addMaxquantPSM(addMaxquantCore(new Features(), features), features);
+      F.add(COL_PSMID, Long.parseLong(F.getStr(COL_PSMID)));
       F.add(COL_AI_APEX,  features.getDouble("Intensity"));
-      F.add(COL_RT_BOUND, Range.closed(
-          mScan_RT.get(features.getInt("Min scan number"), features.get("Raw file")),
-          mScan_RT.get(features.getInt("Max scan number"), features.get("Raw file"))));
-      F.add(COL_SCAN_BOUND, Range.closed(features.getInt("Min scan number"), features.getInt("Max scan number")));
-      F.add(COL_RUN,      features.get("Raw file"));
 
-      if (Strs.isSet(features.get("Sequence")))
-      {
-        F.add(COL_SEQ,  features.get("Sequence"));
-        F.add(COL_PEPTIDE, features.get("Modified sequence"));
-        F.add(COL_PROT, features.get("Proteins"));
-        if (Strs.isSet(features.get("Modifications"))) F.add(COL_MOD,  features.get("Modifications"));
-      }
+//      Double mz = features.getDouble("m/z"), rt = features.getDouble("Retention time");
+//      // add the features
+//      Features F = new Features();
+//
+//      F.add(COL_MZ, mz).add(COL_RT, rt);
+//      F.add(COL_Z,        features.getInt("Charge"));
+//      F.add(COL_MASS,     features.getDouble("Mass"));
+//      F.add(COL_PPM,     features.getDouble("Mass precision [ppm]"));
+//      F.add(COL_AI_APEX,  features.getDouble("Intensity"));
+//      F.add(COL_RT_BOUND, Range.closed(
+//          mScan_RT.get(features.getInt("Min scan number"), features.get("Raw file")),
+//          mScan_RT.get(features.getInt("Max scan number"), features.get("Raw file"))));
+//      F.add(COL_SCAN_BOUND, Range.closed(features.getInt("Min scan number"), features.getInt("Max scan number")));
+//      F.add(COL_RUN,      features.get("Raw file"));
+//      F.add(COL_PSMID,    features.getInt("MS/MS IDs"));
+//      F.add(COL_SCORE,    features.getInt("Score"));
+//
+//      if (Strs.isSet(features.get("Sequence")))
+//      {
+//        F.add(COL_SEQ,  features.get("Sequence"));
+//        F.add(COL_PEPTIDE, features.get("Modified sequence"));
+//        F.add(COL_PROT, features.get("Proteins"));
+//        if (Strs.isSet(features.get("Modifications"))) F.add(COL_MOD,  features.get("Modifications"));
+//      }
 
-      mIons.put(rt, mz, F);
+      mIons.put(F.getDouble(COL_RT), F.getDouble(COL_MZ), F);
 
       if (++counts%10000==0) System.out.print(".");
       if (counts%1000000==0) System.out.println();
@@ -320,6 +409,21 @@ public class LcMsMsFeatures implements Binary
     System.out.println(mPeptides.keySet().size()+" Peptide clusters based on " + mIons.size() + " charge clusters");
     return mPeptides;
   }
+  public void Peptides2Ions()
+  {
+    if (Tools.isSet(mIons) && Tools.isSet(mPSMs))
+      for (Features F : mIons.values())
+      {
+        Long id = F.getLong(COL_PSMID);
+        if (id!=null && mPSMs.containsKey(id))
+        {
+          F.add(COL_MASS_OBS,         F.getDouble(COL_MASS));
+          F.add(COL_MASS, mPSMs.get(id).getDouble(COL_MASS));
+          F.add(COL_PPM,  mPSMs.get(id).getDouble(COL_PPM));
+        }
+      }
+  }
+
   public void Peptides2MsMs(OffsetPpmTolerance mz)
   {
     int ms1=0, ms2=0, pep=0;
@@ -473,13 +577,20 @@ public class LcMsMsFeatures implements Binary
     return df.init(false);
   }
 
-  public TreeBasedTable<Double, Double, Features> indexing()
+  public Map<String, TreeBasedTable<Double, Double, Features>> indexing()
   {
-    TreeBasedTable<Double, Double, Features>  RtAiIons = TreeBasedTable.create();
+    Map<String, TreeBasedTable<Double, Double, Features>>  RunRtAiIons = new TreeMap<>();
     for (Features F : mIons.values())
-      if (F.get(COL_SEQ)!=null) RtAiIons.put(F.getDouble(COL_RT), F.getDouble(COL_AI_APEX), F);
+    {
+      TreeBasedTable<Double, Double, Features> RtAiIons = RunRtAiIons.get(F.get(COL_RUN));
+      if (RtAiIons==null)
+      {
+        RtAiIons = TreeBasedTable.create(); RunRtAiIons.put((String )F.get(COL_RUN), RtAiIons);
+      }
+      if (F.get(COL_PEPTIDE)!=null) RtAiIons.put(F.getDouble(COL_RT), F.getDouble(COL_AI_APEX), F);
+    }
 
-    return RtAiIons;
+    return RunRtAiIons;
   }
   public SortedMap<Double, Features> peering(TreeBasedTable<Double, Double, Features>  RtAiIons, Features F,
                                               double dRT, SortedMap<Double, Features> AiIons)
@@ -493,7 +604,49 @@ public class LcMsMsFeatures implements Binary
 
     return AiIons;
   }
+  public Multimap<String, Features> peers(TreeBasedTable<Double, Double, Features>  RtAiIons, Features F,
+                                          double dRT, Multimap<String, Features> PepIons,
+                                          Multimap<Double, Features>  ppmIons)
+  {
+    if (PepIons==null) PepIons = TreeMultimap.create(); else PepIons.clear();
+    if (ppmIons==null) ppmIons = TreeMultimap.create(); else ppmIons.clear();
+    // gather the peers
+    SortedMap<Double, Map<Double, Features>> slice = RtAiIons.rowMap().subMap(F.getDouble(COL_RT)-dRT, F.getDouble(COL_RT)+dRT);
+    if (slice!=null)
+      for (Map<Double, Features> s : slice.values())
+        for (Features f : s.values())
+          if (!s.containsValue(F)/* && f.get(LcMsMsFeatures.COL_PEPTIDE)!=null*/)
+          {
+            PepIons.put(f.getStr(LcMsMsFeatures.COL_PEPTIDE), f);
+            Tools.put(ppmIons, f.getDouble(LcMsMsFeatures.COL_PPM),  f);
+          }
 
+    return PepIons;
+  }
+  public SortedMap<Double, Features> peer2(TreeBasedTable<Double, Double, Features>  RtAiIons, Features F,
+                                             double dRT, SortedMap<Double, Features> AiIons)
+  {
+    if (AiIons==null) AiIons = new TreeMap<>(Ordering.natural().reversed()); else AiIons.clear();
+    // gather the peers
+    SortedMap<Double, Map<Double, Features>> slice = RtAiIons.rowMap().subMap(F.getDouble(COL_RT)-dRT, F.getDouble(COL_RT)+dRT);
+    if (slice!=null)
+      for (Map<Double, Features> s : slice.values())
+        if (!s.containsValue(F)) AiIons.putAll(s);
+
+    return AiIons;
+  }
+
+  public static Double mean(Collection<Features> fs, String col)
+  {
+    if (!Tools.isSet(fs)) return null;
+
+    Collection<Double> nums = new ArrayList<>();
+    for (Features f : fs)
+      Tools.add(nums, f.getDouble(col));
+
+    Double avg = Stats.mean(nums); nums=Tools.dispose(nums);
+    return avg;
+  }
   @Override
   public void write(DataOutput ds) throws IOException
   {
