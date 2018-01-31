c7a13efd47c29c9fa450e3c22ccab9ae67235990
Yu, Wen <yuw@MedImmune.com>
2018-01-30 21:09:41 -0500

    Group update on Jan 29, 2018

--- src/org/ms2ms/io/CustomFastaProteinReader.java
+++ src/org/ms2ms/io/CustomFastaProteinReader.java
@@ -8,8 +8,6 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.Reader;
 import java.util.EnumMap;
-import java.util.HashMap;
-import java.util.Map;
 
 import static org.ms2ms.io.CustomFastaProteinReader.SeqInfo.*;
 
@@ -38,7 +36,7 @@ public class CustomFastaProteinReader extends FastaProteinReader
     builder.setAccessionId(header.substring(left).replaceAll("\\|", "_"));
   }
 
-  public static EnumMap<SeqInfo, String> parseUniprotHeader(String header)
+  public static EnumMap<SeqInfo, String> parseUniprotHeader(String header, boolean decoy)
   {
     if (!Strs.isSet(header)) return null;
 
@@ -46,7 +44,10 @@ public class CustomFastaProteinReader extends FastaProteinReader
 
     // sp_O75396_SC22B_HUMAN Vesicle-trafficking protein SEC22b OS=Homo sapiens GN=SEC22B PE=1 SV=4
     int r0 = header.indexOf(" "), r1 = header.indexOf("OS=");
-    if (r0>0)          parcels.put(AC,   header.substring(0, r0));
+    parcels.put(AC, r0>0?header.substring(0, r0):header);
+    // tag the decoy if not already
+    if (decoy && parcels.get(AC).indexOf("DECOY")<0) parcels.put(AC, "DECOY_"+parcels.get(AC));
+
     if (r0>0 && r1>r0) parcels.put(DESC, header.substring(r0, r1).trim());
 
     String[] items = r1>0? Strs.split(header.substring(r1), ' '):null;
@@ -54,7 +55,7 @@ public class CustomFastaProteinReader extends FastaProteinReader
       for (String item : items)
       {
         String[] nv=Strs.split(item, '=', true);
-        if (nv!=null && nv.length>1 && "GN".equals(nv[0])) parcels.put(GN, nv[1]);
+        if (nv!=null && nv.length>1 && "GN".equals(nv[0])) parcels.put(GN, decoy?"_"+nv[1]+"_":nv[1]);
         if (nv!=null && nv.length>1 && "OS".equals(nv[0])) parcels.put(OS, nv[1].trim());
       }
 
